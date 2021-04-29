d0a18f852a6e8478708d821ddd1cd49695a8955a
ArtIPok <apokora@itsap.pl>
2021-04-29 17:26:12 +0200

    Add arrow to Brand component

--- src/components/features/Brand/Brand.js
+++ src/components/features/Brand/Brand.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Brand.module.scss';
+import Button from '../../common/Button/Button';
 
 class Brand extends React.Component {
   render() {
@@ -8,14 +9,24 @@ class Brand extends React.Component {
 
     return (
       <div className={styles.root}>
-        <div className={styles.component}>
-          <div className='container'>
-            <div className={`row ${styles.marksRow}`}>
+        <div className='container'>
+          <div className={`row ${styles.marksRow}`}>
+            <div className={styles.brandBox}>
+              <div className={`col ${styles.thumbnail}`}>
+                <Button className={styles.arrowButton}>
+                  <i className={[styles.arrow, styles.left].join(' ')}></i>
+                </Button>
+              </div>
               {brands.map(item => (
                 <div key={item.id} className={`col ${styles.mark}`}>
                   <img className={styles.photo} alt='brand_mark' src={item.mark}></img>
                 </div>
               ))}
+              <div className={`col ${styles.thumbnail}`}>
+                <Button className={styles.arrowButton}>
+                  <i className={[styles.arrow, styles.right].join(' ')}></i>
+                </Button>
+              </div>
             </div>
           </div>
         </div>
