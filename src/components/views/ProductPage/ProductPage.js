219e0b37dfd7b3e8a4a7b6fc2bd52299f2668902
stiffelbo <partygrouppl@gmail.com>
2021-05-01 21:30:17 +0200

    class component

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,9 +1,45 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
 
-const ProductPage = () => <div className={styles.root}>This is ProductPage</div>;
+class ProductPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: null,
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        id: this.props.match.url.replace('/product/', ''),
+      });
+    }, 2500);
+  }
 
-// ProductPage.propTypes = {};
+  render() {
+    return (
+      <div className={`container ${styles.root}`}>
+        <h3>
+          This is ProductPage of: <span>{this.state.id}</span>
+        </h3>
+        <div className={`row`}>
+          <div className={`col-md-6`}>
+            <h5>Product Gallery Here</h5>
+          </div>
+          <div className={`col-md-6`}>
+            <h5>Product Description Here</h5>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ProductPage.propTypes = {
+  match: {
+    url: PropTypes.string,
+  },
+};
 
 export default ProductPage;
